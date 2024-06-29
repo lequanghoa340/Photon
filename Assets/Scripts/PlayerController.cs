b3c40d13b337b66f1d35f6b559ad054aae84ed0c
lequanghoa340 <125871175+lequanghoa340@users.noreply.github.com>
2024-06-29 09:37:03 +0700

    commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    PhotonView view;
+    private void Start()
+    {
+       view = GetComponent<PhotonView>(); 
+    }
+
+    private void Update()
+    {
+        if (view.IsMine)
+        {
+            Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+            Vector2 moveAmount = moveInput.normalized * speed * Time.deltaTime;
+            transform.position += (Vector3)moveAmount;
+        }
+    }
+}
