3ff5e1526d9ce134ee26454943669e0e92a79d11
lequanghoa340 <125871175+lequanghoa340@users.noreply.github.com>
2024-07-11 11:44:12 +0700

    commit

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,9 +7,16 @@ public class PlayerController : MonoBehaviour
 {
     public float speed;
     PhotonView view;
+    Animator anim;
+    Health healthScript;
+
+    LineRenderer rend;
     private void Start()
     {
        view = GetComponent<PhotonView>(); 
+       anim = GetComponent<Animator>();
+       healthScript = FindObjectOfType<Health>();
+        rend = FindObjectOfType<LineRenderer>();
     }
 
     private void Update()
@@ -19,6 +26,35 @@ public class PlayerController : MonoBehaviour
             Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
             Vector2 moveAmount = moveInput.normalized * speed * Time.deltaTime;
             transform.position += (Vector3)moveAmount;
+
+            if (moveInput == Vector2.zero)
+            {
+                anim.SetBool("isRunning", false);
+            }
+            else
+            {
+                anim.SetBool("isRunning", true);
+            }
+
+            //Line
+            rend.SetPosition(0, transform.position);
         }
+        else
+        {
+            rend.SetPosition(1, transform.position);
+        }
+    }
+
+
+    //Xu ly Health
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (view.IsMine)
+        {
+            if (collision.tag == "Enemy")
+            {
+                healthScript.TakeDamage();
+            }
+        }  
     }
 }
